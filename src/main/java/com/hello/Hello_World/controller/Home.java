63485ea14ee0dbcbfbf43918c8faa8e9cf7b5d0c
Babji Polisetti <62492997+babji-dev@users.noreply.github.com>
2026-03-20 13:40:57 +0530

    fix: auto-repair syntax_error [Pipeline Guardian] [17dbad4a]

--- src/main/java/com/hello/Hello_World/controller/Home.java
+++ src/main/java/com/hello/Hello_World/controller/Home.java
@@ -1,15 +1,2 @@
-package com.hello.Hello_World.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class Home {
-
-    @GetMapping()
     public String hello(){
-            1
         return "I'm Working!!";
-    }
-
-}
