7b022e51b53132b47664a103ce9e5bc488390d4c
Babji Polisetti <babji7819@gmail.com>
2026-03-24 11:09:52 +0530

    Test Controller

---
+++ src/main/java/com/hello/Hello_World/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.hello.Hello_World.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test3")
+    public String hello2(){
+        return "I'm Working!!";
+    }
+
+
+}
--- src/test/java/com/hello/Hello_World/HelloWorldApplicationTests.java
+++ src/test/java/com/hello/Hello_World/HelloWorldApplicationTests.java
@@ -1,7 +1,6 @@
 package com.hello.Hello_World;
 
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
