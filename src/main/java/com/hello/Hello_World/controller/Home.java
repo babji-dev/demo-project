383120cd5efd801af6c836a54e49960f9c01a944
Babji Polisetti <62492997+babji-dev@users.noreply.github.com>
2026-03-31 15:22:28 +0530

    fix: auto-repair syntax_error [Pipeline Guardian] [104f529a]

--- src/main/java/com/hello/Hello_World/controller/Home.java
+++ src/main/java/com/hello/Hello_World/controller/Home.java
@@ -5,12 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Home {
-    @GetMappng()
+    @GetMapping()
     public String hello(){
         return "I'm Working!!";
     }
-    @GetMapping("/test2")
-    public String hello2(){        return "I'm Working!!";  
+    @GetMapping("/test2")    public String hello2(){        return "I'm Working!!";  
     }
 
 }
